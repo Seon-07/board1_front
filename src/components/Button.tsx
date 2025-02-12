34a0e2bc16985d23d1801a11ff907058d0e60f20
seon <tjswlghks0722@naver.com>
2025-02-12 22:17:46 +0900

    update
    update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,22 +2,27 @@
 
 interface ButtonProps {
     text: string;
-    color?: 'blue' | 'green' | 'red';
+    color?: 'blue' | 'green' | 'red' | 'gray';
+    onClick?: () => void;
+    className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, color = 'blue' }) => {
+const Button: React.FC<ButtonProps> = ({ text, color = 'blue', onClick, className}) => {
     let buttonClass = 'px-4 py-2 text-white rounded-lg';
     
     if (color === 'blue') {
-        buttonClass += ' bg-blue-500 hover:bg-blue-600';
+        buttonClass += ' bg-blue-500 hover:bg-blue-700';
     } else if (color === 'green') {
-        buttonClass += ' bg-green-500 hover:bg-green-600';
+        buttonClass += ' bg-green-500 hover:bg-green-700';
     } else if (color === 'red') {
-        buttonClass += ' bg-red-500 hover:bg-red-600';
+        buttonClass += ' bg-red-500 hover:bg-red-700';
+    } else if (color === 'gray') {
+        buttonClass += ' bg-gray-500 hover:bg-gray-700';
     }
-
+    if(className) buttonClass += ' ' + className;
+    
     return (
-        <button className={buttonClass}>
+        <button className={buttonClass} onClick={onClick}>
             {text}
         </button>
     );
--- src/pages/BoardList.tsx
+++ src/pages/BoardList.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 interface Board {
     id: string;
@@ -10,7 +11,7 @@ interface Board {
 
 const BoardList: React.FC = () => {
     const [boardList, setBoardList] = useState<Board[]>([]);
-
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get('http://localhost:8080/board/list')
             .then(response => {
@@ -24,22 +25,24 @@ const BoardList: React.FC = () => {
 
     return (
         <div>
-            <h1>게시판</h1>
+            <div className="flex justify-center items-center py-8">
+                <Button text="이전" color="gray" onClick={() => navigate(`/`)} className="absolute top-4 left-4"/>
+                <div className="text-4xl">게시판</div>
+            </div>
+            <div className="flex justify-center">
             {boardList.length > 0 ? (
-                <table>
+                <table className="w-3/4 border-collapse border border-gray-300">
                     <thead>
                         <tr>
-                            <th>제목</th>
-                            <th>작성자</th>
+                            <th className="py-4 w-4/5">제목</th>
+                            <th className="py-4 w-1/5">작성자</th>
                         </tr>
                     </thead>
                     <tbody>
                     {boardList.map(board => (
-                        <tr key={board.id}>
-                            <td>
-                                <Link to={`/board/${board.id}`}>{board.title}</Link>
-                            </td>
-                            <td>{board.author}</td>
+                        <tr key={board.id} className="border border-gray-200 hover:bg-gray-50 cursor-pointer" onClick={() => navigate(`/board/${board.id}`)}>
+                            <td className="py-2 px-4">{board.title}</td>
+                            <td className="py-2 px-4 text-center">{board.author}</td>
                         </tr>
                     ))}
                     </tbody>
@@ -47,6 +50,7 @@ const BoardList: React.FC = () => {
             ) : (
                 <p>데이터를 불러오는 중입니다...</p>
             )}
+            </div>
         </div>
     );
 }
